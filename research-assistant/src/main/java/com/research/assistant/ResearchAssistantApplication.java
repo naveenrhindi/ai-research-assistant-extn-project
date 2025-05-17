cba80de61da0ba8bcde6af37518e02914835bf01
naveenrhindi <naveenhindi4@gmail.com>
2025-05-17 18:11:04 +0530

    First Commit

---
+++ research-assistant/src/main/java/com/research/assistant/ResearchAssistantApplication.java
@@ -0,0 +1,16 @@
+package com.research.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchAssistantApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+
+}
